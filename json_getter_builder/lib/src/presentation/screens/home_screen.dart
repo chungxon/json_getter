20e750cbb1a9fdf55574f425ac697b28aa3f02d8
Le Trung Son <lesonlhld@gmail.com>
2025-01-02 00:49:46 +0700

    feat: add json getter steps section

--- json_getter_builder/lib/src/presentation/screens/home_screen.dart
+++ json_getter_builder/lib/src/presentation/screens/home_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:json_getter/json_getter.dart';
 import 'package:provider/provider.dart';
 
 import '../state/home_viewmodel.dart';
+import 'widgets/json_getter_section.dart';
 import 'widgets/json_initialization_section.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -23,13 +25,51 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
       body: Consumer<HomeViewmodel>(
         builder: (context, model, child) {
-          return const Padding(
-            padding: EdgeInsets.all(16.0),
+          final disableAddNewFilter = model.isLoadingJson ||
+              model.rawJson.isEmpty ||
+              (model.filters.filters.isNotEmpty &&
+                  !TypeUtils.isJson(
+                    model.filterJsons.entries.lastOrNull?.value ?? '',
+                  ));
+
+          return Padding(
+            padding: const EdgeInsets.all(16.0),
             child: SingleChildScrollView(
               child: Column(
                 spacing: 16,
                 children: [
-                  JsonInitializationSection(),
+                  const JsonInitializationSection(),
+                  for (var i = 0; i < model.filters.filters.length; i++)
+                    JsonGetterSection(
+                      index: i,
+                      filter: model.filters.filters[i],
+                      rawJson: i == 0
+                          ? model.rawJson
+                          : model.filterJsons[i - 1] ?? '',
+                      outputJson: model.filterJsons[i],
+                      onFilterChanged: (filter, json) {
+                        model.updateFilters(i, filter, json);
+                      },
+                    ),
+                  Row(
+                    spacing: 8,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      FilledButton.icon(
+                        onPressed:
+                            disableAddNewFilter ? null : model.addNewFilter,
+                        label: const Text('Add Filter'),
+                        icon: const Icon(Icons.add_rounded),
+                      ),
+                      FilledButton.icon(
+                        onPressed: model.filters.filters.isEmpty
+                            ? null
+                            : model.removeLastFilter,
+                        label: const Text('Remove Last Filter'),
+                        icon: const Icon(Icons.remove_rounded),
+                      ),
+                    ],
+                  ),
                 ],
               ),
             ),
